7521e54e03e4c5625dfcecdc3986591e57c49968
Flo Mincucci <florencia@mincucci.com.ar>
2012-11-06 01:52:24 -0300

    Working chat. Debugging tic tac toe

---
+++ espejo.py
@@ -0,0 +1,91 @@
+from gevent import monkey; monkey.patch_all()
+
+from socketio import socketio_manage
+from socketio.server import SocketIOServer
+from socketio.namespace import BaseNamespace
+from socketio.mixins import RoomsMixin, BroadcastMixin
+
+
+class EspejoNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
+    def on_nickname(self, nickname):
+        self.request['nicknames'].append(nickname)
+        self.socket.session['nickname'] = nickname
+        self.broadcast_event('announcement', '%s has connected' % nickname)
+        self.broadcast_event('nicknames', self.request['nicknames'])
+        # Just have them join a default-named room
+        self.join('main_room')
+
+    def recv_disconnect(self):
+        # Remove nickname from the list.
+        nickname = self.socket.session['nickname']
+        self.request['nicknames'].remove(nickname)
+        self.broadcast_event('announcement', '%s has disconnected' % nickname)
+        self.broadcast_event('nicknames', self.request['nicknames'])
+
+        self.disconnect(silent=True)
+
+    def on_user_message(self, msg):
+        self.emit_to_room('main_room', 'msg_to_room',
+            self.socket.session['nickname'], msg)
+
+    def on_move(self, movement):
+        self.emit_to_room('main_room', 'new_movement',
+            self.socket.session['nickname'], movement)
+
+    def recv_message(self, message):
+        print "PING!!!", message
+
+    def recv_move(self, movement):
+        print "ACK", movement
+
+class Application(object):
+    def __init__(self):
+        self.buffer = []
+        # Dummy request object to maintain state between Namespace
+        # initialization.
+        self.request = {
+            'nicknames': [],
+        }
+
+    def __call__(self, environ, start_response):
+        path = environ['PATH_INFO'].strip('/')
+
+        if not path:
+            start_response('200 OK', [('Content-Type', 'text/html')])
+            return ['<h1>Welcome. '
+                'Del otro lado del  <a href="/espejo.html">espejo</a>...</h1>']
+
+        if path.startswith('js/') or path.startswith('css/') or path == 'espejo.html':
+            try:
+                data = open(path).read()
+            except Exception:
+                return not_found(start_response)
+
+            if path.endswith(".js"):
+                content_type = "text/javascript"
+            elif path.endswith(".css"):
+                content_type = "text/css"
+            elif path.endswith(".swf"):
+                content_type = "application/x-shockwave-flash"
+            else:
+                content_type = "text/html"
+
+            start_response('200 OK', [('Content-Type', content_type)])
+            return [data]
+
+        if path.startswith("socket.io"):
+            socketio_manage(environ, {'/espejo': EspejoNamespace}, self.request)
+        else:
+            return not_found(start_response)
+
+
+def not_found(start_response):
+    start_response('404 Not Found', [])
+    return ['<h1>Not Found</h1>']
+
+
+if __name__ == '__main__':
+    print 'Escuchando en los puertos 8080 y 843 (flash policy server)'
+    SocketIOServer(('0.0.0.0', 8080), Application(),
+        resource="socket.io", policy_server=True,
+        policy_listener=('0.0.0.0', 10843)).serve_forever()
