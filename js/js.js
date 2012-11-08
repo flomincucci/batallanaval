6f596a1a95ac641c41c45f4e12e68a49b8b1832a
Flo Mincucci <florencia@mincucci.com.ar>
2012-11-08 02:25:12 -0300

    Emprolijando

---
+++ js/js.js
@@ -0,0 +1,38 @@
+// jQueryUI fun
+
+$(function() {
+
+	$("#controles input").button();
+	$("#losbotones").buttonset();
+
+});
+
+// DOM manipulation
+$(function () {
+		$('#send-message').submit(function () {
+			message('me', $('#message').val());
+			socket.emit('user message', $('#message').val());
+			clear();
+			$('#lines').get(0).scrollTop = 10000000;
+			return false;
+		});
+
+		$("#tateti input").click(function () {
+			var position = $(this).parent().parent().attr('class') + '|' + $(this).parent().attr('class');
+			socket.emit('move',position);
+		});
+
+		$("#controles input").bind({
+			'mousedown': function() {
+				socket.emit('pressed',$(this).attr('id'));
+			},
+			'mouseup': function() {
+				socket.emit('notpressed',$(this).attr('id'));
+			},
+		});
+
+		function clear () {
+			$('#message').val('').focus();
+		};
+});
+
---
+++ js/socket-stuff.js
@@ -0,0 +1,58 @@
+WEB_SOCKET_SWF_LOCATION = "/js/WebSocketMain.swf";
+WEB_SOCKET_DEBUG = true;
+
+// socket.io specific code
+var socket = io.connect("/espejo");
+
+socket.on('connect', function () {
+	socket.emit('nickname','alice');
+});
+
+socket.on('announcement', function (msg) {
+	$('#lines').append($('<p>').append($('<em>').text(msg)));
+});
+
+socket.on('nicknames', function (nicknames) {
+	$('#nicknames').empty().append($('<span>Online: </span>'));
+	for (var i in nicknames) {
+		$('#nicknames').append($('<b>').text(nicknames[i]));
+	}
+});
+
+socket.on('msg_to_room', message);
+socket.on('new_movement', move);
+socket.on('input_pressed', pressed);
+socket.on('input_unpressed', unpressed);
+
+socket.on('reconnect', function () {
+	$('#lines').remove();
+	message('System', 'Reconnected to the server');
+});
+
+socket.on('reconnecting', function () {
+	message('System', 'Attempting to re-connect to the server');
+});
+
+socket.on('error', function (e) {
+	message('System', e ? e : 'A unknown error occurred');
+});
+
+function message (from, msg) {
+	$('#lines').append($('<p>').append($('<b>').text(from), msg));
+}
+
+function move (from, movement) {
+	var row = movement.substring(0,8);
+	var col = movement.substring(9,19);
+	$('.' + row + ' > .' + col + ' > input').attr('checked','checked');
+}
+
+function pressed (from, element) {
+	$('#' + element + '').addClass('ui-state-active');	
+}
+
+function unpressed (from, element) {
+	$('#' + element + '').removeClass('ui-state-active');	
+}
+
+
